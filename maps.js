6a596456ad225b310ad1d8ca960061355af5eda1
Carin Hansen <carin@burst-digital.com>
2019-06-25 14:53:24 +0200

    feat(maps) add markers and new position to map

--- maps.js
+++ maps.js
@@ -1,34 +1,10 @@
-let light, police, userOne, userTwo, marker;
-
-// let lightButton = document.querySelector('.button__light');
-// let policeButton = document.querySelector('.button__police');
-// let userButton = document.querySelector('.button__user');
-// let lightBox = document.querySelector('.button__yellow');
-// let policeBox = document.querySelector('.button__blue');
-// let userBox = document.querySelector('.button__green');
-// lightBox.classList.toggle('button__yellow--clicked')
-//
-// lightButton.addEventListener('click', () => {
-//   lightBox.classList.toggle('button__yellow--clicked')
-//   light.setMap(light.getMap() ? null : map);
-// });
-//
-// policeButton.addEventListener('click', () => {
-//   policeBox.classList.toggle('button__blue--clicked')
-//   police.setMap(police.getMap() ? null : map);
-// });
-//
-// userButton.addEventListener('click', () => {
-//   userBox.classList.toggle('button__green--clicked')
-//   userOne.setMap(userOne.getMap() ? null : map);
-//   userTwo.setMap(userTwo.getMap() ? null : map);
-// });
+let light;
 
 let map;
 
 function initMap() {
 
-  let myLatLng = {lat: 51.9170588, lng: 4.484828};
+  let myLatLng = {lat: 51.9038695, lng: 4.4855031};
 
   map = new google.maps.Map(document.getElementById('map'), {
     zoom: 16,
@@ -36,13 +12,26 @@ function initMap() {
     mapTypeId: 'terrain'
   });
 
+
   let marker = new google.maps.Marker({
-    position: myLatLng,
+    position: {lat: 51.9050127, lng: 4.4847497},
     map: map,
-    title: 'Map marker'
+    title: 'Map marker',
+    icon: './images/marker-02.png'
   });
 
   marker.addListener('click', function() {
+    window.location = '/choices';
+  });
+
+  let marker2 = new google.maps.Marker({
+    position: {lat: 51.9041871, lng: 4.4857913},
+    map: map,
+    title: 'Humphreys',
+    icon: './images/marker-03.png'
+  });
+
+  marker2.addListener('click', function() {
     window.location = '/choices';
   });
 
